21f0a435a054e9c3c77cc20a9299c0e2f59e62ae
Philippe-Nomena <philippenomena@gmail.com>
2024-05-13 16:12:07 +0200

    premier routes

--- server/app.js
+++ server/app.js
@@ -3,17 +3,21 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const PratiquantsRoute = require("./routes/pratiquantsRoute");
 
+// middlewares
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes
+app.use("/pratiquants", PratiquantsRoute);
 
 app.listen(process.env.APP_PORT, process.env.URL, () => {
   console.log("======================================");
