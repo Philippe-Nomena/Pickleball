1aa5c628cdb396db2aab7cc66c5f81ad507a7db4
Philippe-Nomena <philippenomena@gmail.com>
2024-05-27 15:17:02 +0200

    test de backend activité

--- server/models/Activite.js
+++ server/models/Activite.js
@@ -1,58 +1,26 @@
-// const Sequelize = require("sequelize");
-// const database = require("../utils/database");
+const Sequelize = require("sequelize");
+const database = require("../utils/database");
 
-// const Activite = database.define("activite", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     allowNull: false,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   nom: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   }
-// });
-
-// database
-//   .sync()
-//   .then(() => {
-//     console.log("Creation du table activite avec succes!");
-//   })
-//   .catch((error) => {
-//     console.error("creation du table activite echoue :", error);
-//   });
-
-// module.exports = Activite;
-// models/Activite.js
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
-
-class Activite extends Model {}
-
-Activite.init(
-  {
-    nom: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    groupe: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    prix: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    horaire: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+const Activite = database.define("activite", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    modelName: "Activite",
-  }
-);
+  nom: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+database
+  .sync()
+  .then(() => {
+    console.log("Creation du table activite avec succes!");
+  })
+  .catch((error) => {
+    console.error("creation du table activite echoue :", error);
+  });
 
 module.exports = Activite;
--- views/activity/activity.js
+++ views/activity/activity.js
@@ -16,34 +16,36 @@ import url from "../url";
 
 const Activity = () => {
   const [data, setData] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
+  // const [selectedItem, setSelectedItem] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
   const [ajoutModal, setAjoutModal] = useState(false);
   const [editedItem, setEditedItem] = useState(null);
   const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [categorieModalVisible, setCategorieModalVisible] = useState(false);
   const [itemToDelete, setItemToDelete] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
   const [nom, setNom] = useState("");
-  const [categories, setCategories] = useState([""]);
+  const [activiteId, setActiviteId] = useState("");
+  const [categories, setCategories] = useState("");
   const [groupe, setGroupe] = useState([]);
   const [horaire, SetHoraire] = useState("");
   const [prix, setPrix] = useState("");
-  const handleAddCategory = () => {
-    if (categories.length < 4) {
-      setCategories([...categories, ""]);
-    }
-  };
-  const handleRemoveCategory = (index) => {
-    if (categories.length > 1) {
-      const newCategories = categories.filter((_, i) => i !== index);
-      setCategories(newCategories);
-    }
-  };
-  const handleCategoryChange = (text, index) => {
-    const newCategories = [...categories];
-    newCategories[index] = text;
-    setCategories(newCategories);
-  };
+  // const handleAddCategory = () => {
+  //   if (categories.length < 4) {
+  //     setCategories([...categories, ""]);
+  //   }
+  // };
+  // const handleRemoveCategory = (index) => {
+  //   if (categories.length > 1) {
+  //     const newCategories = categories.filter((_, i) => i !== index);
+  //     setCategories(newCategories);
+  //   }
+  // };
+  // const handleCategoryChange = (text, index) => {
+  //   const newCategories = [...categories];
+  //   newCategories[index] = text;
+  //   setCategories(newCategories);
+  // };
   useEffect(() => {
     fetchAllData();
   }, []);
@@ -86,29 +88,30 @@ const Activity = () => {
   const handleEdit = (item) => {
     setEditedItem(item);
     setNom(item.nom);
-    setCategories(item.categories);
-    setGroupe(item.groupe);
-    SetHoraire(item.horaire);
-    setPrix(item.prix);
+
     setModalVisible(true);
   };
-
+  const handleCategorie = (item) => {
+    setEditedItem(item);
+    setActiviteId(item.id);
+    setCategorieModalVisible(true);
+  };
   const saveEditedItem = async () => {
     try {
       await url.put(`/activite/${editedItem.id}`, {
         nom,
-        groupe,
-        categories,
-        horaire,
-        prix,
+        // groupe,
+        // categories,
+        // horaire,
+        // prix,
       });
       setModalVisible(false);
       fetchAllData();
       setNom("");
-      setCategories([""]);
-      setGroupe([]);
-      SetHoraire("");
-      setPrix("");
+      // setCategories([""]);
+      // setGroupe([]);
+      // SetHoraire("");
+      // setPrix("");
       Alert.alert("Edit réussi");
     } catch (error) {
       console.error("Error editing item:", error);
@@ -118,14 +121,17 @@ const Activity = () => {
 
   const addActivity = async () => {
     try {
-      await url.post("/activite", { nom, categories, groupe, horaire, prix });
+      await url.post("/activite", {
+        nom,
+        // , categories, groupe, horaire, prix
+      });
       setAjoutModal(false);
       fetchAllData();
       setNom("");
-      setCategories([""]);
-      setGroupe([]);
-      SetHoraire("");
-      setPrix("");
+      // setCategories([""]);
+      // setGroupe([]);
+      // SetHoraire("");
+      // setPrix("");
       Alert.alert("Ajout d'activité réussi avec succès");
     } catch (error) {
       console.error("Error adding activity:", error);
@@ -139,6 +145,12 @@ const Activity = () => {
 
   const renderRightActions = (item) => (
     <View style={tw`flex-row mr-5`}>
+      <TouchableOpacity
+        style={tw`bg-blue-500 p-2 h-10 mr-1 rounded-md`}
+        onPress={() => handleCategorie(item)}
+      >
+        <MaterialIcons name="category" size={24} color="black" />
+      </TouchableOpacity>
       <TouchableOpacity
         style={tw`bg-blue-500 p-2 h-10 mr-1 rounded-md`}
         onPress={() => handleEdit(item)}
@@ -218,7 +230,7 @@ const Activity = () => {
                 />
               </View>
 
-              <View>
+              {/* <View>
                 {categories.map((category, index) => (
                   <View key={index} style={tw`mb-2 `}>
                     <Text style={tw`text-white mb-2`}>Categorie:</Text>
@@ -331,7 +343,7 @@ const Activity = () => {
                     </View>
                   </View>
                 </View>
-              </View>
+              </View> */}
             </ScrollView>
 
             <View style={tw`flex-row justify-center items-center`}>
@@ -375,14 +387,14 @@ const Activity = () => {
                 onPress={confirmDeleteItem}
               >
                 <Entypo name="trash" size={18} color="white" />
-                <Text style={tw`text-white text-center ml-1`}>Delete</Text>
+                <Text style={tw`text-white text-center ml-1`}>Supprimer</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={tw`bg-gray-500 p-2 rounded-md w-20 flex-row`}
                 onPress={() => setDeleteModalVisible(false)}
               >
                 <MaterialIcons name="cancel" size={20} color="white" />
-                <Text style={tw`text-white text-center ml-1`}>Cancel</Text>
+                <Text style={tw`text-white text-center ml-1`}>Annuler</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -415,7 +427,7 @@ const Activity = () => {
                 />
               </View>
 
-              <View>
+              {/* <View>
                 {categories.map((category, index) => (
                   <View key={index} style={tw`mb-2 `}>
                     <Text style={tw`text-white mb-2`}>Categorie:</Text>
@@ -530,7 +542,7 @@ const Activity = () => {
                     </View>
                   </View>
                 ))}
-              </View>
+              </View> */}
             </ScrollView>
 
             <View style={tw`flex-row justify-center items-center`}>
@@ -548,6 +560,127 @@ const Activity = () => {
                 <MaterialIcons name="cancel" size={20} color="white" />
                 <Text style={tw`text-white text-center ml-2`}>Annuler</Text>
               </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={categorieModalVisible}
+        onRequestClose={() => setCategorieModalVisible(!categorieModalVisible)}
+      >
+        <View>
+          <View style={tw`mb-2 `}>
+            <Text style={tw`text-white mb-2`}>Categorie:</Text>
+            <View style={tw`flex-row items-center`}>
+              <TextInput value={activiteId} name="activitesId" />
+              <TextInput
+                style={tw`bg-gray-200 border border-gray-600 rounded-md p-2 text-center w-44`}
+                placeholder="ex:A || debutant ..."
+                value={categories}
+                onChangeText={setCategories}
+                name="categories"
+                // value={category}
+                // onChangeText={(text) => handleCategoryChange(text, index)}
+              />
+              {/* {index === 0 && (
+                <TouchableOpacity
+                  onPress={handleAddCategory}
+                  style={tw`bg-blue-900 ml-2 rounded-xl p-2`}
+                >
+                  <AntDesign name="plus" size={24} color="white" />
+                </TouchableOpacity>
+              )}
+              {index > 0 && (
+                <TouchableOpacity
+                  style={tw`bg-red-900 ml-2 rounded-xl p-2`}
+                  onPress={() => handleRemoveCategory(index)}
+                >
+                  <AntDesign name="minus" size={24} color="white" />
+                </TouchableOpacity>
+              )} */}
+            </View>
+          </View>
+        </View>
+
+        <View>
+          <Text style={tw`text-white `}>Horaire:</Text>
+          <TextInput
+            style={tw`bg-gray-200 border border-gray-600 rounded-md p-2 mb-2 text-center`}
+            placeholder="ex: 9h-10h-30"
+            value={horaire}
+            onChangeText={SetHoraire}
+            name="horaire"
+          />
+        </View>
+
+        <View>
+          <Text style={tw`text-white `}>Prix:</Text>
+          <TextInput
+            style={tw`bg-gray-200 border border-gray-600 rounded-md p-2 mb-2 text-center`}
+            placeholder="Prix"
+            value={prix}
+            onChangeText={setPrix}
+            name="prix"
+          />
+        </View>
+        <View style={tw`mt-4`}>
+          <Text style={tw`text-white text-lg font-bold mb-2`}>Jours</Text>
+          <View style={tw`flex-row`}>
+            <View style={tw`flex-col mr-1`}>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Lundi")}
+                  onChange={() => handleSetGroupe("Lundi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Lundi</Text>
+              </View>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Mardi")}
+                  onChange={() => handleSetGroupe("Mardi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Mardi</Text>
+              </View>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Mercredi")}
+                  onChange={() => handleSetGroupe("Mercredi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Mercredi</Text>
+              </View>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Jeudi")}
+                  onChange={() => handleSetGroupe("Jeudi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Jeudi</Text>
+              </View>
+            </View>
+
+            <View style={tw`flex-col`}>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Vendredi")}
+                  onChange={() => handleSetGroupe("Vendredi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Vendredi</Text>
+              </View>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Samedi")}
+                  onChange={() => handleSetGroupe("Samedi")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Samedi</Text>
+              </View>
+              <View style={tw`flex-row items-center mb-2`}>
+                <Checkbox
+                  checked={groupe.includes("Dimanche")}
+                  onChange={() => handleSetGroupe("Dimanche")}
+                />
+                <Text style={tw`text-white text-lg ml-2`}>Dimanche</Text>
+              </View>
             </View>
           </View>
         </View>
