b78a3653e7fb160d3295ae429f696d8c0752eb2c
Philippe-Nomena <philippenomena@gmail.com>
2024-06-05 13:20:49 +0200

    ajout upload sur middleware et mise à jour sur l'activite Route

--- server/routes/activiteRoute.js
+++ server/routes/activiteRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const ActiviteController = require("../controllers/ActiviteController");
+const upload = require("../middlewares/upload");
 
 router.post("/", upload.single("image"), ActiviteController.createActivite);
 router.get("/", ActiviteController.getAllActivite);
 router.get("/:id", ActiviteController.getActivite);
 router.delete("/:id", ActiviteController.deleteActivite);
-router.put("/:id", ActiviteController.updateActivite);
+router.put("/:id", upload.single("image"), ActiviteController.updateActivite);
 
 module.exports = router;
