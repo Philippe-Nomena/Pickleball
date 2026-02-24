619fcd6081d4e0710035b99aeef1282be6a0a8c2
Philippe-Nomena <philippenomena@gmail.com>
2026-02-24 15:18:57 +0300

    test port

--- server/utils/database.js
+++ server/utils/database.js
@@ -5,7 +5,7 @@ const database = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
   }
 );
